39d89d23e8e664f426a5d8169e9f28ede9571808
stefansimereaa <stefanosimerea16@gmail.com>
2023-06-07 15:47:35 +0200

    fixed code

--- js/script.js
+++ js/script.js
@@ -4,39 +4,54 @@ console.log('JS Ok');
 
 const grid = document.getElementById('grid');
 const button = document.getElementById('play');
+const selectedDifficulty = document.getElementById('difficulty');
 
-
-// Funzione creazione celle
+// Funzione Creazione celle
 const createCell = () => {
-    const cell = document.createElement('div')
+    const cell = document.createElement('div');
     cell.className = 'cell hidden';
+
+    //Aggiunge classe clicked e il numero dentro la cella in console
+    cell.addEventListener('click', function(){
+        cell.classList.add('clicked');
+        console.log(cell.innerText);
+    });
     return cell;
-}
+};
 
 // Dati tabella
-const rows = 10;
-const cols = 10;
+let rows = 10;
+let cols = 10;
 const totalCells = rows * cols;
 
 
+// Click del bottone Play
 button.addEventListener('click', function(){
+    
+    // Selezione Difficoltà
+    const valueDifficulty = selectedDifficulty.value;
+    
+   if (valueDifficulty === `normal`) {
+        rows = 10;
+        cols = 10;
+    } else if (valueDifficulty === `hard`) {
+        rows = 9;
+        cols = 9;
+    } else if (valueDifficulty === `superhard`) {
+        rows = 7;
+        cols = 7;
+    }
 
     const cells = grid.getElementsByClassName('cell');
     for (let i = 0; i < cells.length; i++) {
         cells[i].classList.remove('hidden');
     }
-})
-
+});
 
 // Celle aggiunte nella tabella
 for(let i = 0; i < totalCells; i++){
 
     const cell = createCell();
     cell.innerText = i + 1;
-
-    cell.addEventListener('click', function(){
-        cell.classList.add('clicked');
-        console.log(cell.innerText)
-    })
     grid.appendChild(cell);
 }
---
+++ steps.md
@@ -0,0 +1,16 @@
+# Esercizio Campo minato
+L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+Ogni cella ha un numero progressivo, da 1 a 100.
+Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+- Recupero elementi dal DOM
+- Far  scegliere all'utente la difficulta
+    - Se selezionata la difficulta "normal"
+    creare una griglia 10x10
+    - Se selezionata la difficulta "normal"
+    creare una griglia 9x9
+    - Se selezionata la difficulta "normal"
+    creare una griglia 7x7
+- Al click del pulsante play
+    - Mostrare le celle
